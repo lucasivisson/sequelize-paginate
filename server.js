e2fc560f600d6de79aa6212020d753cbfc85464b
Lucas Ivisson <lucasivisson1@gmail.com>
2023-12-15 15:42:27 -0300

    .

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const app = express()
 const port = 3000
 const User = require('./models/user')
 const { Op } = require('sequelize')
+const moment = require('moment')
 
 app.post('/post', async (req, res) => {
   const user = await User.create({
-    firstName: 'Lais',
-    lastName: 'Lais',
-    email: 'Lais@gmail.com',
+    firstName: 'Ribeiro',
+    lastName: 'Ribeiro',
+    email: 'Ribeiro@gmail.com',
   })
   res.json(user)
 })
@@ -16,20 +17,35 @@ app.post('/post', async (req, res) => {
 app.get('/get', async (req, res) => {
   const page = 1
   const perPage = 5
-  const where = { firstName: { [Op.like]: `%i%` }}
-  const { docs, pages, total } = await User.paginate({
+  // const where = { firstName: { [Op.like]: `%a%` }}
+  console.log(moment('2023-12-15'))
+  const initialDateFormatted = moment('2023-12-14').format('YYYY-MM-DDT14:38:18')
+  const finalDateFormatted = moment('2023-12-14').add(1, 'day').format('YYYY-MM-DDT13:34:34')
+  const where = { 
+    createdAt: {
+      [Op.between]: [initialDateFormatted, finalDateFormatted]
+    }
+  }
+  const paginateResult = await User.paginate({
     page,
     paginate: perPage,
-    order: [['createdAt', 'DESC']],
+    order: [['createdAt', 'ASC']],
     where
   })
+  console.log(paginateResult.docs)
   res.json({
-    page: Number(page), // pagina acessada no momento
-    perPage: Number(perPage), // quantidade de objetos por pagina 
-    total: total, // quantidade de itens no total juntando todas as paginas
-    totalPages: pages, // quantidade total de paginas retornadas
-    data: docs // objetos retornado pela paginação})
-  })
+    count: paginateResult.docs.length,
+    // timesQueried: number,
+    limit: Number(perPage),
+    lastKey: Number(paginateResult.pages) > Number(page) ? Number(page) + 1 : undefined,
+    data: paginateResult.docs})
+  // res.json({
+  //   page: Number(page), // pagina acessada no momento - lastKey
+  //   perPage: Number(perPage), // quantidade de objetos por pagina - limit
+  //   total: total, // quantidade de itens no total juntando todas as paginas - nao precisa
+  //   totalPages: pages, // quantidade total de paginas retornadas
+  //   data: docs // objetos retornado pela paginação}) - dados
+  // })
 })
 
 app.listen(port, () => {
